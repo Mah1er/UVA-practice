9cf9c550a1e2ff0d4db1289677b795ac3d1c9cfc
Mahler <liwangzi123@gmail.com>
2022-08-27 12:07:27 +0800

    Sort

---
+++ Sort/Insertion.c
@@ -0,0 +1,16 @@
+void InsertionSort(int *arr,int size)
+{
+    for(int i=0;i<size-1;i++)
+    {
+        int tmp;
+        for(int j=i;j>0;j--)
+        {
+            if(arr[j-1]>arr[j])
+            {
+                tmp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=tmp;
+            }
+        }
+    }
+}
